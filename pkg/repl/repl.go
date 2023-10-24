e944947d4dabf7df8c234999cb617c78cb4311ba
Naphat Permpredanun <68365175+NaphatPRM@users.noreply.github.com>
2023-10-24 00:02:09 -0400

    Update repl.go
    Adding the REPL Runchan

--- pkg/repl/repl.go
+++ pkg/repl/repl.go
@@ -88,7 +88,41 @@ func (r *REPL) Run(c net.Conn, clientId uuid.UUID, prompt string) {
 
 // Run the REPL.
 func (r *REPL) RunChan(c chan string, clientId uuid.UUID, prompt string) {
-	panic("function not yet implemented")
+	// Get reader and writer; stdin and stdout if no conn.
+	writer := os.Stdout
+	replConfig := &REPLConfig{writer: writer, clientId: clientId}
+	// Begin the repl loop!
+	io.WriteString(writer, prompt)
+	for payload := range c {
+		// Emit the payload for debugging purposes.
+		io.WriteString(writer, payload+"\n")
+		// Parse the payload.
+		fields := strings.Fields(payload)
+		if len(fields) == 0 {
+			io.WriteString(writer, prompt)
+			continue
+		}
+		trigger := cleanInput(fields[0])
+		// Check for a meta-command.
+		if trigger == ".help" {
+			io.WriteString(writer, r.HelpString())
+			io.WriteString(writer, prompt)
+			continue
+		}
+		// Else, check user commands.
+		if command, exists := r.commands[trigger]; exists {
+			// Call a hardcoded function.
+			err := command(payload, replConfig)
+			if err != nil {
+				io.WriteString(writer, fmt.Sprintf("%v\n", err))
+			}
+		} else {
+			io.WriteString(writer, "command not found\n")
+		}
+		io.WriteString(writer, prompt)
+	}
+	// Print an additional line if we encountered an EOF character.
+	io.WriteString(writer, "\n")
 }
 
 // cleanInput preprocesses input to the db repl.
